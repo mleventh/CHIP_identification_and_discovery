4ca50df20f3402632af0849b718f0373c828e187
mleventh <leventhalmatt@gmail.com>
2019-08-01 10:41:36 -0400

    add comments

--- discovery_in_ExAC/phylogic_1d_clustering/purity_estimator_docker/phylogic_preprocess_genome.py
+++ discovery_in_ExAC/phylogic_1d_clustering/purity_estimator_docker/phylogic_preprocess_genome.py
@@ -30,14 +30,19 @@ def main():
 
     vcf=pd.read_csv(input_file,sep='\t',compression='gzip',comment="#")
     afb=np.arange(0,1.01,0.01)
+    
+    #re-format VCF as a phylogic-ready MAF
     vcf.columns=['Chromosome', 'Start_position',"ID",'Reference_Allele','Tumor_Seq_Allele2','QUAL','FILTER',
                 'INFO','FORMAT',sample_name]
 
+    #eliminate chr prefixes
     vcf['Chromosome'].replace(to_replace=r'^chr',value="",regex=True,inplace=True)
 
+    #filter rejected sites
     vcf=vcf[vcf['FILTER'].str.match('PASS')]
     vcf=vcf.reset_index(drop=True)
 
+    #eliminate sites with >1% population allele frequency in ExAC
     vcf=remove_exac_sites_from_vcf(vcf,exac_file)
     vcf=vcf.reset_index(drop=True)
 
@@ -49,6 +54,7 @@ def main():
     vcf['t_ALT_F1R2']=pd.to_numeric(count_info.iloc[:,3].str.split(',',expand=True)[1])
     vcf['t_ALT_F2R1']=pd.to_numeric(count_info.iloc[:,4].str.split(',',expand=True)[1])
 
+    #eliminate orientation bias artifacts and variants with small LOD relative of a mutation rate of .2/Mb
     vcf['orientation_bias_F1']=stats.binom.cdf(pd.to_numeric(vcf.t_ALT_F1R2),
                               pd.to_numeric(vcf.t_ALT_F1R2)+pd.to_numeric(vcf.t_ALT_F2R1),0.96)
     vcf['orientation_bias_F2']=stats.binom.cdf(pd.to_numeric(vcf.t_ALT_F2R1),
@@ -79,6 +85,7 @@ def main():
 
     beta_vals=[]
 
+    #calculate beta pdf for each variant in the defined allele fraction bins
     for idx,row in vcf.iterrows():
         distribution=stats.beta.pdf(afb,row['t_alt_count']+1,row['t_ref_count']+1)
         beta_vals.append(distribution)
@@ -89,6 +96,7 @@ def main():
               "Variant_Classification", "Reference_Allele","Tumor_Seq_Allele1","Tumor_Seq_Allele2","Variant_Type",
               "Tumor_Sample_Barcode")]
 
+    #append beta pdf information
     formatted_maf=pd.concat([vcf,beta_df],axis=1)
 
     formatted_maf.to_csv(output_filename,sep='\t',index=False)
